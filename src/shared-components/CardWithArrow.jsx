553cdde1a605222849d9e0000741e227cd3b5f31
Beth Thomas <beth.thomas@softwire.com>
2024-06-23 14:32:32 +0100

    add hint boxes

--- src/shared-components/CardWithArrow.jsx
+++ src/shared-components/CardWithArrow.jsx
@@ -1,40 +1,35 @@
 
 import { useState } from "react";
-import {ChevronDown, ChevronUp} from "react-bootstrap-icons";
-import {Col, Collapse, Row} from "react-bootstrap";
+import {CardHeading, ChevronDown, ChevronUp} from "react-bootstrap-icons";
+import {Button, Card, CardBody, CardHeader, Col, Collapse, Row} from "react-bootstrap";
 
 const CardWithArrow = ({ title, children }) => {
   const [showCard, setShowCard] = useState(true);
 
   return (
-    <div
-      className={"d-flex align-items-start px-4 pt-3 bg-white " +
-        (showCard ? "pb-3 " : "") +
-        "grey-border rounded-3"}
-    >
-      <div
-        className="w-auto action-header"
-        style={{padding: "16px", margin: "-16px"}}
-        onClick={() => setShowCard(!showCard)}
-      >
-          <Row className={"d-flex flex-row mb-3"}>
-            <Col>
-              <strong style={{fontSize: "16px"}}>{title}</strong>
-            </Col>
-            <Col className={"justify-content-end"}>
-                {showCard ? (
-                  <ChevronUp/>
-                ) : (
-                  <ChevronDown/>
-                )}
-            </Col>
-          </Row>
-      </div>
-      {/*<Collapse in={showCard}>*/}
-      {/*  <div className={"p-0 m-0"}>        <Row>*/}
-      {/*    Hello</Row></div>*/}
-      {/*</Collapse>*/}
-    </div>
+    <Card>
+      <CardHeader as="h5" className="card-heading d-flex" onClick={() => setShowCard(!showCard)}>
+        <Col>
+          <h5>Featured</h5>
+        </Col>
+<Col style={{display: 'flex'}} className={"float-end"} >
+  <ChevronUp className={"float-end"} />
+
+</Col>
+      </CardHeader>
+
+      <Collapse in={showCard}>
+        <div className="m-0 p-0">
+        <CardBody>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </CardBody>
+        </div>
+      </Collapse>
+    </Card>
 
   )
     ;
--- src/shared-components/CodeInputBar.js
+++ src/shared-components/CodeInputBar.js
@@ -1,10 +1,12 @@
-import {Button, Col, Container, Form, Nav, Navbar, Row} from "react-bootstrap";
+import {Alert, Button, Col, Container, Form, Nav, Navbar, Row} from "react-bootstrap";
 import {useState} from "react";
 
 function CodeInputBar({ setShowSuccess }) {
   const [inputtedCode, setInputtedCode] = useState('');
+  const [incorrect, setIncorrect] = useState(false);
 
   const handleChange = (e) => {
+    setIncorrect(false)
     const value = e.target.value.replace(/[^0-9]/g, '');
     if (e.target.value.length <= 4) {
       setInputtedCode(value);
@@ -17,8 +19,10 @@ function CodeInputBar({ setShowSuccess }) {
     if (inputtedCode === '1234') {
       setShowSuccess(true)
       console.log('yay')
-    } else {
-      console.log('oh no!')
+    }
+
+    else {
+      setIncorrect(true)
     }
   };
 
@@ -27,8 +31,9 @@ function CodeInputBar({ setShowSuccess }) {
   return (
     <Container className={"d-flex justify-content-center "}>
       <div className="code-input-wrapper">
-        <p className="text-center">When you think you've got the code,
-          Enter your four digits below.</p>
+        <p className="text-center code-text pb-2"><em>When you think you've got the code,
+          enter your four digits below:</em></p>
+        {incorrect && <Alert variant={"danger"}>Oops, that's not quite right! Try again</Alert> }
 
         <Form className="mw-100" onSubmit={handleSubmit}>
           <Row className="pb-4"><Form.Control type={"number"}
